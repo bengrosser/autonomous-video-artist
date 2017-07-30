1bc3c619bfc8e132853f5d3dd3e805975d6aef5e
Xinyu Zhang <zhangxinyu.tju@gmail.com>
2017-07-29 22:43:28 -0500

    reorganize the code

---
+++ robot/sys_info/src/sys_publisher.cpp
@@ -0,0 +1,30 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+#include <sys/sysinfo.h>
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+    ros::init(argc, argv, "publisher");
+    ros::NodeHandle n;
+
+    ros::Publisher sys_pub = n.advertise<std_msgs::Int32>("sys/RAM", 1000);
+    ros::Rate loop_rate(10);
+
+    int count = 0;    
+    while(ros::ok())
+    {
+        struct sysinfo si;
+        sysinfo(&si);
+        std_msgs::Int32 msg;
+        msg.data = si.freeram;
+        sys_pub.publish(msg);
+        ros::spinOnce();
+        loop_rate.sleep();
+        ++count;
+        cout<<count<<" loop"<<endl;
+    }
+    
+    return 0;
+}
