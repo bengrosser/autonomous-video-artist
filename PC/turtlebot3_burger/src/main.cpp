9b1f1561fb2ce67536339536c4ce74374bbb9ee4
xzhan140 <zhangxinyu.tju@gmail.com>
2018-09-04 02:22:44 -0500

    update navigation code for burger

--- PC/turtlebot3_burger/src/main.cpp
+++ PC/turtlebot3_burger/src/main.cpp
@@ -7,6 +7,7 @@
 #include <tf/tf.h>
 #include <tf/LinearMath/Transform.h>
 #include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
 #include <signal.h>
 
 
@@ -36,13 +37,21 @@ class AutoNav
 		
 		bool move_forward;
 		float LIDAR_ERR;
+		double yaw;
+
+		tf::StampedTransform transform;
+		tf::TransformListener listener;
 
 	public:
 		AutoNav(ros::NodeHandle& handle):node(handle), move_forward(true), LIDAR_ERR(0.05)
 		{
 			stop = false;
 			velocity = node.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+			listener.waitForTransform("/odom", "/base_footprint", ros::Time(0), ros::Duration(1.0));
 			signal(SIGINT, SigintHandler);
+
+
+
 			//ros::MultiThreadedSpinner threads(2);
 
 			ros::Subscriber scan = node.subscribe<sensor_msgs::LaserScan>("/scan", 10, &AutoNav::LaserInfo, this);
@@ -91,39 +100,73 @@ class AutoNav
 				//cout<<"go straight"<<endl;
 			}
 			else{
-				/*geometry_msgs::Twist decision;
-				decision.linear.x = 0.0;
-				decision.angular.z = 0.1;
-				velocity.publish(decision);
+				/*velocity.publish(decision);
 				tf::StampedTransform transform;
+				tf::TransformListener listener;
+				listener.lookupTransform()
 				tf::Quaternion q = transform.getRotation();
 				double yaw = tf::getYaw(q);*/
 
 				geometry_msgs::Twist decision;
 				int random = rand()%2;
 				if(random == 0){
-					/*int prev = 20;
-					for(int i = 21; i < 360; ++i){
+					cout<<"clockwise"<<endl;
+					int prev = 20;
+					for(int i = 21; i < 340; ++i){
 						if(i-prev > 40)
 							break;
 						if(scan->ranges[i] > LIDAR_ERR && scan->ranges[i] < 0.3)
 							prev = i;
 					}
 					double target = ((float)prev+20.0)/180.0*pi;
-					while(yaw < target)*/
+					if(target > pi)
+						target = target-2*pi;
+					get_odom();
+					cout<<"yaw: "<<yaw<<endl;
 					decision.linear.x = 0.0;
 					decision.angular.z = 0.2;
+					while(abs(yaw - target) > 0.05 && !stop){
+						velocity.publish(decision);
+						get_odom();
+						cout<<"clockwise yaw: "<<yaw<<endl;
+						cout<<"target: "<<target<<endl;
+					}
+
+					/*decision.linear.x = 0.0;
+					decision.angular.z = 0.2;*/
 				}
 				else{
+					cout<<"anti-clockwise"<<endl;
+					int prev = 340;
+					for(int i = 339; i >= 20; --i){
+						if(prev - i > 40)
+							break;
+						if(scan->ranges[i] > LIDAR_ERR && scan->ranges[i] < 0.3)
+							prev = i;
+					}
+					double target = ((float)prev-20.0)/180.0*pi;
+					if(target > pi)
+						target = target-2*pi;
+					get_odom();
+					cout<<"yaw: "<<yaw<<endl;
 					decision.linear.x = 0.0;
 					decision.angular.z = -0.2;
+					while(abs(yaw - target) > 0.05 && !stop){
+						velocity.publish(decision);
+						get_odom();
+						cout<<"anti-clockwise yaw: "<<yaw<<endl;
+						cout<<"target: "<<target<<endl;
+					}
+
+					/*decision.linear.x = 0.0;
+					decision.angular.z = -0.2;*/
 				}
-				ros::Time current_time = ros::Time::now();
+				/*ros::Time current_time = ros::Time::now();
 				while(ros::Time::now()-current_time <= ros::Duration(3.0) && !stop){
 					cout<<"in the while loop"<<endl;
 					velocity.publish(decision);
 					cout<<"turn around"<<endl;
-				}
+				}*/
 				if(stop){
 					decision.linear.x = 0.0;
 					decision.angular.z = 0.0;
@@ -137,6 +180,12 @@ class AutoNav
 		{
 
 		}*/
+
+		void get_odom(){
+			listener.lookupTransform("/odom", "/base_footprint", ros::Time(0), transform);
+			tf::Quaternion q = transform.getRotation();
+			yaw = tf::getYaw(q);
+		}
 };
 
 
